0f49a81be52bf1627d49a7e5677374281d02072d
Rob Muhlestein <rob@rwx.gg>
2022-10-08 17:05:11 -0400

    Add termcolor shortcuts

--- weeks/19/termcolors/termcolors.go
+++ weeks/19/termcolors/termcolors.go
@@ -6,18 +6,29 @@ import (
 	"time"
 )
 
-const Black = "\033[30m"
-const Red = "\033[31m"
-const Green = "\033[32m"
-const Yellow = "\033[33m"
-const Blue = "\033[34m"
-const Magenta = "\033[35m"
-const Cyan = "\033[36m"
-const White = "\033[37m"
-const Reset = "\033[0m"
-const Clear = "\033[H\033[2J"
-const CurOff = "\033[?25l"
-const CurOn = "\033[?25h"
+const (
+	Black   = "\033[30m"
+	K       = Black
+	Red     = "\033[31m"
+	R       = Red
+	Green   = "\033[32m"
+	G       = Green
+	Yellow  = "\033[33m"
+	Y       = Yellow
+	Blue    = "\033[34m"
+	B       = Blue
+	Magenta = "\033[35m"
+	M       = Magenta
+	Cyan    = "\033[36m"
+	C       = Cyan
+	White   = "\033[37m"
+	W       = White
+	Reset   = "\033[0m"
+	X       = Reset
+	Clear   = "\033[H\033[2J"
+	CurOff  = "\033[?25l"
+	CurOn   = "\033[?25h"
+)
 
 // Rand returns a random color ANSI escape between Black (30) and White
 // (37).
--- weeks/19/termcolors/termcolors_test.go
+++ weeks/19/termcolors/termcolors_test.go
@@ -30,6 +30,30 @@ func ExampleBlack() {
 	// "\x1b[37mwhite\x1b[0m"
 }
 
+func ExampleBlack_shortcuts() {
+
+	// C "github.com/rwxrob/boost/weeks/19/termcolors"
+
+	fmt.Printf("%q\n", C.K+"black"+C.X)
+	fmt.Printf("%q\n", C.R+"red"+C.X)
+	fmt.Printf("%q\n", C.G+"green"+C.X)
+	fmt.Printf("%q\n", C.Y+"yellow"+C.X)
+	fmt.Printf("%q\n", C.B+"blue"+C.X)
+	fmt.Printf("%q\n", C.M+"magenta"+C.X)
+	fmt.Printf("%q\n", C.C+"cyan"+C.X)
+	fmt.Printf("%q\n", C.W+"white"+C.X)
+
+	// Output:
+	// "\x1b[30mblack\x1b[0m"
+	// "\x1b[31mred\x1b[0m"
+	// "\x1b[32mgreen\x1b[0m"
+	// "\x1b[33myellow\x1b[0m"
+	// "\x1b[34mblue\x1b[0m"
+	// "\x1b[35mmagenta\x1b[0m"
+	// "\x1b[36mcyan\x1b[0m"
+	// "\x1b[37mwhite\x1b[0m"
+}
+
 func ExampleRand() {
 	fmt.Printf("%q", C.Rand())
 }
